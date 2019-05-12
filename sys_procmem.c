089e2d12bd965497796ba28e12f7011eb9b38699
quantrancse <quan.tran.itbk@hcmut.edu.vn>
2019-05-12 08:15:18 -0700

    Update sys_procmem

---
+++ sys_procmem.c
@@ -0,0 +1,32 @@
+#include <linux/linkage.h>
+#include <linux/sched.h>
+
+struct proc_segs {
+  unsigned long student_ID;
+  unsigned long start_code;
+  unsigned long end_code;
+  unsigned long start_data;
+  unsigned long end_data;
+  unsigned long start_heap;
+  unsigned long end_heap;
+  unsigned long start_stack;
+}
+
+asmlinkage long sys_procmem(int pid, struct proc_segs *info) {
+  info->student_ID = 1752044;
+  struct task_struct *task;
+  for_each_process(task) {
+    if (task->pid == pid) {
+      info->start_code = task->mm->start_code;
+      info->end_code = task->mm->end_code;
+      info->start_data = task->mm->start_data;
+      info->end_data = task->mm->end_data;
+      info->start_heap = task->mm->start_brk;
+      info->end_heap = task->mm->brk;
+      info->start_stack = task->mm->start_stack;
+      return 0;
+    }
+  }
+  return -1;
+}
+
