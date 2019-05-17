3ef1be1d131c46c4380fff948b56fd2c0c40a3d7
quantrancse <quan.tran.itbk@hcmut.edu.vn>
2019-05-17 08:40:09 -0700

    Update

--- sys_procmem.c
+++ sys_procmem.c
@@ -10,21 +10,23 @@ struct proc_segs {
   unsigned long start_heap;
   unsigned long end_heap;
   unsigned long start_stack;
-}
+};
 
 asmlinkage long sys_procmem(int pid, struct proc_segs *info) {
   info->studentID = 1752044;
   struct task_struct *task;
   for_each_process(task) {
     if (task->pid == pid) {
-      info->start_code = task->mm->start_code;
-      info->end_code = task->mm->end_code;
-      info->start_data = task->mm->start_data;
-      info->end_data = task->mm->end_data;
-      info->start_heap = task->mm->start_brk;
-      info->end_heap = task->mm->brk;
-      info->start_stack = task->mm->start_stack;
-      return 0;
+      if (task->mm != NULL) {
+        info->start_code = task->mm->start_code;
+        info->end_code = task->mm->end_code;
+        info->start_data = task->mm->start_data;
+        info->end_data = task->mm->end_data;
+        info->start_heap = task->mm->start_brk;
+        info->end_heap = task->mm->brk;
+        info->start_stack = task->mm->start_stack;
+        return 0;
+      }
     }
   }
   return -1;
